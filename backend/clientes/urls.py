76e96338d0d90b3a9b1788e4a98e931ad52ccd6c
Cristian Isaac Vargas <cristian.v62@gmail.com>
2026-07-17 15:41:40 -0300

    feat(clientes): configurar enrutamiento para el detalle del cliente en urls.py

--- backend/clientes/urls.py
+++ backend/clientes/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import CrearClienteView
+from .views import CrearClienteView, DetalleClienteView
 
 urlpatterns = [
     path('', CrearClienteView.as_view(), name='crear-cliente'),
+    path('<uuid:pk>/', DetalleClienteView.as_view(), name='detalle-cliente'),
 ]
